5736cff40025cf29839a2471c5d8c2027e7e20d4
Shyam-S7 <shyamselvam275@gmail.com>
2025-10-18 21:37:49 +0530

    d

--- test_app.py
+++ test_app.py
@@ -2,7 +2,6 @@ from app import app
 
 
 def test_home():
-    response = app.test_client().get("/")
-
+    response = app.test_client().get("/home")
     assert response.status_code == 200
-    assert response.data == b"hi everyone"
+    assert response.data == b"hello everyone"
